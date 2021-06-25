61dc3d0196dba6d613f78e58185b3d39fc3b3f39
Kitsune Solar <kitsune.solar@gmail.com>
2021-06-25 11:59:01 +0300

    2021-06-25 08:59:01

--- mirror.sh
+++ mirror.sh
@@ -16,8 +16,8 @@ mirror() {
     SOURCE="https://src.fedoraproject.org/rpms/${i}.git"
     TARGET="https://${USER}:${TOKEN}@https://github.com/mirror-rpm/${i}.git"
 
-    ${git} clone "${SOURCE}" "/root/git/${i}" && pushd "/root/git/${i}" || exit 1
-    ${git} remote add 'target' "${TARGET}"  \
+    ${git} clone --mirror "${SOURCE}" "/root/git/${i}" && pushd "/root/git/${i}" || exit 1
+    ${git} remote add 'target' "${TARGET}"
     popd || exit 1
   done
 }
